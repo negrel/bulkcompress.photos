d36340d2e1246c6c224515cb42583f522fdf13d8
Alexandre Negrel <alexandre@negrel.dev>
2024-01-04 14:27:06 +0100

    remove dead code

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -9,7 +9,6 @@ export default function App({ Component }: PageProps) {
         <title>Bulk compress photos</title>
         <link rel="stylesheet" href="/styles.css" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-        <script type="module" src="/mod.mjs"></script>
       </head>
       <body className="bg-slate-50 dark:bg-slate-950 text-slate-950 dark:text-slate-50">
         <Component />
