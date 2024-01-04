4cb64c9c87332a9256774f1b63cb2880320d253e
Alexandre Negrel <alexandre@negrel.dev>
2024-01-04 19:34:42 +0100

    improve 404 page

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -6,14 +6,14 @@ export default function Error404() {
       <Head>
         <title>404 - Page not found</title>
       </Head>
-      <div class="px-4 py-8 mx-auto bg-[#86efac]">
+      <div class="px-4 py-8 mx-auto dark:bg-slate-800 bg-slate-200">
         <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
           <img
             class="my-6"
             src="/logo.svg"
             width="128"
             height="128"
-            alt="the Fresh logo: a sliced lemon dripping with juice"
+            alt="Bulk photos compressor logos: a photos icon"
           />
           <h1 class="text-4xl font-bold">404 - Page not found</h1>
           <p class="my-4">
